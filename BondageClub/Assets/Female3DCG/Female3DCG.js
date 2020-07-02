455c41417f4994a05e3d05ccf3fbe59735956187
Sarukami <51291632+BunnyMadMan@users.noreply.github.com>
2020-07-02 17:42:36 +0200

    Update Female3DCG.js

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -486,7 +486,9 @@ var AssetFemale3DCG = [
 			{ Name: "Crown2", Value: 20 },
 			{ Name: "Crown3", Value: 20 },
 			{ Name: "Crown4", Value: 20 },
-			{ Name: "Crown5", Value: 20 }
+			{ Name: "Crown5", Value: 20 },
+			{ Name: "Veil1", Value: 1 },
+			{ Name: "Veil2", Value: 1 }
 		],
 		Color: ["Default", "#202020", "#808080", "#bbbbbb", "#aa8080", "#80aa80", "#8080aa", "#aaaa80", "#80aaaa", "#aa80aa", "#cc3333", "#33cc33", "#3333cc", "#cccc33", "#33cccc", "#cc33cc"]
 	},
